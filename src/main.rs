7a4d0ea81946c317004f29170279c608ecc95dd5
buhe <bugu1986@126.com>
2021-11-10 19:45:58 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,16 @@ fn main() -> std::io::Result<()> {
 }
 
 mod tests {
+    #[test]
     fn integer() {
-
+        lang_study::run("int main() {
+    return 2021;
+}".to_string(), &mut std::io::stdout()).unwrap();
     }
-
+    #[test]
     fn neg_integer() {
-        
+        lang_study::run("int main() {
+    return --2021;
+}".to_string(), &mut std::io::stdout()).unwrap();
     }
 }
