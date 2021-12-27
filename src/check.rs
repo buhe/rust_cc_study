664e4a0d16b07a04910309c20d3fbd5bb52b0c55
buhe <bugu1986@126.com>
2021-12-27 13:45:06 +0800

    step5

--- src/check.rs
+++
@@ -1,5 +0,0 @@
-use crate::ast::Prog;
-
-pub fn check(p: &Prog) -> &Prog {
-    p
-}
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ pub mod ast;
 pub mod ir;
 pub mod codegen;
 pub mod regeister;
-pub mod check;
 pub mod symbols;
 
 use lexer::tokenize;
@@ -16,7 +15,6 @@ pub fn run(path: String, output: &mut impl Write) -> Result<()> {
   println!("Tokens: {:#?}", t);
    let mut p = parsing(&t);
      println!("Prog: {:#?}", &p.0);
-   //   let p = check::check(&p.0);
      let p_ir = ir::ast2ir(&p.0, &mut p.1);
      println!("IR Prog: {:#?}", &p_ir);
      codegen::write_asm(&p_ir,&mut p.1, output)
